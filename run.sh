f81930d003fd6cbad549c4d6da2e4a41fc168285
atm <tamaya0228@gmail.com>
2024-09-24 16:46:32 +0900

    modified run.sh

--- run.sh
+++ run.sh
@@ -7,6 +7,11 @@
 # module load cuda/12.3.2  cudnn/9.0.0 
 # module load ffmpeg/6.1.1
 
- python run.py \
-     --input_rgb_dir input/in-the-wild_example \
-     --output_dir output/in-the-wild_example_lcm
+source ~/venv/marigold/bin/activate
+#pip list
+python run.py \
+    --checkpoint prs-eth/marigold-v1-0 \
+    --denoise_steps 50 \
+    --ensemble_size 10 \
+    --input_rgb_dir input/in-the-wild_example \
+    --output_dir output/in-the-wild_example
